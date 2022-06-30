f1adb3bcfba4f2f8fb761bebbfac3a65e7327062
Bunyameen <bunyameen@gmail.com>
2022-06-30 16:23:00 +0100

    unicafe step6.1 - 6.2

---
+++ unicafe-redux/src/reducer.js
@@ -0,0 +1,23 @@
+const initialState = {
+  good: 0,
+  ok: 0,
+  bad: 0
+}
+
+const counterReducer = (state = initialState, action) => {
+  console.log(action, state)
+  switch (action.type) {
+    case 'GOOD':
+      return {...state, good: state.good + action.payload}
+    case 'OK':
+      return {...state, ok: state.ok +  action.payload}
+    case 'BAD':
+      return {...state, bad: state.bad +  action.payload}
+    case 'ZERO':
+      return initialState
+    default: return state
+  }
+  
+}
+
+export default counterReducer
